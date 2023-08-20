bf401d504dac7eb37852c562d7ca0947cee7e90e
rayctx <45115112+rayctx@users.noreply.github.com>
2023-08-20 23:51:20 +0700

    Update ItemsSeeder.php

--- database/seeders/ItemsSeeder.php
+++ database/seeders/ItemsSeeder.php
@@ -45,6 +45,111 @@ class ItemsSeeder extends Seeder
                 'kinds_id' => 2,
                 'stock' => '75',
                 'is_deleted' => false
+            ],
+            [
+                'id' => 6,
+                'item_name' => 'Telur',
+                'kinds_id' => 1,
+                'stock' => '100',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 7,
+                'item_name' => 'Minyak',
+                'kinds_id' => 1,
+                'stock' => '76',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 8,
+                'item_name' => 'Beras',
+                'kinds_id' => 1,
+                'stock' => '80',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 9,
+                'item_name' => 'Mi Instan',
+                'kinds_id' => 1,
+                'stock' => '22',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 10,
+                'item_name' => 'Sikat Gigi',
+                'kinds_id' => 2,
+                'stock' => '33',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 11,
+                'item_name' => 'Sabun Cuci',
+                'kinds_id' => 2,
+                'stock' => '75',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 12,
+                'item_name' => 'Sabun Wajah',
+                'kinds_id' => 2,
+                'stock' => '76',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 13,
+                'item_name' => 'Tisu Wajah',
+                'kinds_id' => 2,
+                'stock' => '130',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 14,
+                'item_name' => 'Sagu',
+                'kinds_id' => 1,
+                'stock' => '70',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 15,
+                'item_name' => 'Tisu Basah',
+                'kinds_id' => 2,
+                'stock' => '75',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 16,
+                'item_name' => 'Nugget',
+                'kinds_id' => 1,
+                'stock' => '75',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 17,
+                'item_name' => 'Susu',
+                'kinds_id' => 1,
+                'stock' => '98',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 18,
+                'item_name' => 'Tepung',
+                'kinds_id' => 1,
+                'stock' => '80',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 19,
+                'item_name' => 'Cabai Bubuk',
+                'kinds_id' => 1,
+                'stock' => '70',
+                'is_deleted' => false
+            ],
+            [
+                'id' => 20,
+                'item_name' => 'Madu',
+                'kinds_id' => 1,
+                'stock' => '75',
+                'is_deleted' => false
             ]
         );
         DB::table('items')->truncate();
